7889c38a352151da2e4249ec83b4da0948330d43
Pedro Caribe <pho.caribe@gmail.com>
2024-04-03 21:56:18 -0300

    reworked requests and added local DB for faster retrieval of images

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route("/", methods=['GET', 'POST'])
-def index():
+async def index():
     """Show index page"""
     return render_template("index.html", gn1="Summoner 1 Game Name", gn2="Summoner 2 Game Name", tg1="Tag", tg2="Tag", failed_game_name_1=False, failed_game_name_2=False)
 
@@ -52,35 +52,61 @@ async def search():
     puuid_1 = req1.json()['puuid'] if req1 else False
 
     end1 = time.time()
-    time_process_player__info_1 = round(end1 - start1, 2)
+    time_process_player_info_1 = round(end1 - start1, 2)
+    print("Time spent to get game name and tag line player 1:", time_process_player_info_1)
 
     start2 = time.time()
     req2 = requests.get(f'{ACCOUNT_V1}{game_name_2}/{tag_line_2}?api_key={RIOT_TOKEN}')
     puuid_2 = req2.json()['puuid'] if req2 else False
 
     end2 = time.time()
-    time_process_player__info_2 = round(end2 - start2, 2)
-
-    #account_id = requests.get(SUMMONER_V4+puuid+'?api_key='+RIOT_TOKEN).json()['accountId']
+    time_process_player_info_2 = round(end2 - start2, 2)
+    print("Time spent to get game name and tag line player 2:", time_process_player_info_2)
 
     if puuid_1 and puuid_2:
 
+
+        # Retrieve current version of LOL
+        VERSION_API = "https://ddragon.leagueoflegends.com/api/versions.json"
+        latest_version = requests.get(VERSION_API).json()[0]
+
+
+        # Retrieve champions from Riot API
+        CHAMPIONS_URL = f"https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/en_US/champion.json"
+        champions = requests.get(CHAMPIONS_URL).json()["data"]
+
+
+        # Retrieve spells from Riot API
+        SPELLS_URL = f"https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/en_US/summoner.json"
+        spells = requests.get(SPELLS_URL).json()["data"]
+
+        # Generic Items URL for icon images
+        ITEMS_URL = f"http://ddragon.leagueoflegends.com/cdn/{latest_version}/img/item/"
+
+
+        # Retrieve list of past 100 games for Player 1
         start3 = time.time()
         matches_player_1 = requests.get(f'{MATCH_V5}by-puuid/{puuid_1}/ids?start=0&count=100&api_key={RIOT_TOKEN}').json()
         end3 = time.time()
         time_process_player_1_matches = round(end3 - start3, 2)
+        print("Time spent to get matches player 1:", time_process_player_1_matches)
 
+        # Retrieve list of past 100 games for Player 2
         start4 = time.time()
         matches_player_2 = requests.get(f'{MATCH_V5}by-puuid/{puuid_2}/ids?start=0&count=100&api_key={RIOT_TOKEN}').json()
         end4 = time.time()
         time_process_player_2_matches = round(end4 - start4, 2)
+        print("Time spent to get matches player 2:", time_process_player_2_matches)
 
+        # Join both lists of matches and check if interesects
         played_with = list((set(matches_player_1).intersection(matches_player_2)))
 
         if played_with:
 
             match_stats = list()
 
+            # Retrieve game information for each match intersected
+            start5 = time.time()
             for each in played_with:
                 req = requests.get(f'{MATCH_V5}{each}?api_key={RIOT_TOKEN}').json()
 
@@ -94,8 +120,11 @@ async def search():
                     "p1":p1_stats_per_match,
                     "p2":p2_stats_per_match
                 })
+            end5 = time.time()
+            time_process_matches_info = round(end5 - start5, 2)
+            print("Time spent to matches info:", time_process_matches_info)
 
-            async def date_sort(e):
+            def date_sort(e):
                 return e['data']['info']['gameCreation']
 
             match_stats.sort(reverse=True, key=date_sort)
@@ -107,89 +136,69 @@ async def search():
                 item_numbers_p1 = [each['p1'][f'item{i}'] for i in range(6)]
                 item_numbers_p2 = [each['p2'][f'item{i}'] for i in range(6)]
 
-                async def get_latest_ddragon(package="champion"):
-                    version_url = "https://ddragon.leagueoflegends.com/api/versions.json"
-                    latest_version = await requests.get(version_url).json()[0]
 
-                    if package == "champion":
-                        champions_url = f"https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/en_US/champion.json"
-                        
-                        ddragon = await requests.get(champions_url)
-                        champions = ddragon.json()["data"]
-
-                        return champions, latest_version
-        
-                    if package == "spell":
-                        spells_url = f"https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/en_US/summoner.json"
-
-                        ddragon = await requests.get(spells_url)
-                        spells = ddragon.json()["data"]
-
-                        return spells, latest_version
-
-                        
-                
                 async def get_champions_by_key(key):
-                    champions, latest_version = await get_latest_ddragon("champion")
-
+                    
                     for champion in champions:
                         if champions[champion]["key"] == str(key):
-                            return champions[champion], latest_version
+                            return champions[champion]
                 
                 async def get_spell_by_key(key):
-                    spells, latest_version = await get_latest_ddragon("spell")
-
+                    
                     for spell in spells:
                         if spells[spell]["key"] == str(key):
-                            return spells[spell], latest_version
+                            return spells[spell]
                         
 
 
                 async def get_image_path(key_number, package=None):
                     
                     if package == "champion":
-                        
-                        champion, latest_version = await get_champions_by_key(key_number)
+ 
+                        champion = await get_champions_by_key(key_number)
+ 
                         champion_name = champion["id"]
 
                         base_url = f"https://ddragon.leagueoflegends.com/cdn/{latest_version}/img/champion/"
 
-                        return f"{base_url}/{champion_name}"
+                        return f"{base_url}{champion_name}.png"
                     
                     if package == "spell":
                         
-                        spell, latest_version = await get_spell_by_key(key_number)
+                        spell = await get_spell_by_key(key_number)
+
                         spell_name = spell["id"]
 
                         base_url = f"http://ddragon.leagueoflegends.com/cdn/{latest_version}/img/spell/"
 
-                        return f"{base_url}/{spell_name}"
+                        return f"{base_url}{spell_name}.png"
                     
+                    if package == "item":
+                        return f"{ITEMS_URL}{key_number}.png"
+                        
                     return None
                 
 
-                                                                    # PARA OS ITENS
+                async def get_image_list(item_numbers):
+                    image_paths = []
+                    for item_number in item_numbers:
+                        if item_number != 0:
+                            image_path = await get_image_path(item_number, "item")
+                            if image_path:
+                                image_paths.append(image_path)
+                    
+                    return image_paths
+                
+                image_list_p1 = await get_image_list(item_numbers_p1)
+                image_list_p2 = await get_image_list(item_numbers_p2)
 
 
-                                                                    # async def get_image_list(item_numbers, folder_path="./static/items/"):
-                                                                    #     image_paths = []
-                                                                    #     for item_number in item_numbers:
-                                                                    #         image_path = get_image_path(item_number)
-                                                                    #         if image_path:
-                                                                    #             image_paths.append(image_path)
-                                                                        
-                                                                    #     return image_paths
-                                                                    
-                                                                    # image_list_p1 = get_image_list(item_numbers_p1)
-                                                                    # image_list_p2 = get_image_list(item_numbers_p2)
-
-
-                champion_image_p1 = get_image_path(each['p1']['championId'], "champion")
-                champion_image_p2 = get_image_path(each['p2']['championId'], "champion")
-                s1_summ1_img = get_image_path(each['p1']['summoner1Id'], "spell")
-                s1_summ2_img = get_image_path(each['p1']['summoner2Id'], "spell")
-                s2_summ1_img = get_image_path(each['p2']['summoner1Id'], "spell")
-                s2_summ2_img = get_image_path(each['p2']['summoner2Id'], "spell")
+                champion_image_p1 = await get_image_path(each['p1']['championId'], "champion")
+                champion_image_p2 = await get_image_path(each['p2']['championId'], "champion")
+                s1_summ1_img = await get_image_path(each['p1']['summoner1Id'], "spell")
+                s1_summ2_img = await get_image_path(each['p1']['summoner2Id'], "spell")
+                s2_summ1_img = await get_image_path(each['p2']['summoner1Id'], "spell")
+                s2_summ2_img = await get_image_path(each['p2']['summoner2Id'], "spell")
 
                 match_info.append({
                     "match_id":each['data']['metadata']['matchId'],
