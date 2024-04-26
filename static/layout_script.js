2baf8ce3dedec718f7b3cd67836902d1494dd77a
Pedro Caribe <pho.caribe@gmail.com>
2024-04-26 13:53:26 -0300

    no comment

--- static/layout_script.js
+++ static/layout_script.js
@@ -10,12 +10,16 @@ document.getElementById('contact-button').addEventListener('click', function() {
             const name = document.getElementById('swal-input1').value;
             const email = document.getElementById('swal-input2').value;
             const message = document.getElementById('swal-input3').value;
+
+            // Following RFC-2822
+            let reg_pattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
             if (!email || !message) {
                 Swal.showValidationMessage("Please enter email and message");
                 return false;
             }
 
-            else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)))
+            else if (!(reg_pattern.test(email)))
             {
                 Swal.showValidationMessage("Please enter a valid email address")
                 return false;
