1fd7b3134e26b1e907f865a8280f3f16f6d24a18
Pedro Caribe <pho.caribe@gmail.com>
2024-04-29 02:06:11 -0300

    improved development/production modes

--- settings.py
+++ settings.py
@@ -3,19 +3,14 @@ from pathlib import Path
 from dotenv import load_dotenv
 from colorama import Back, Fore,Style
 
-os.environ['DEBUG'] = '1' # Remove for Production Mode
 
-DEBUG = os.getenv('DEBUG', False)
+env_path = Path('.') / ('.env.debug' if os.getenv('FLASK_ENV') == 'development' else '.env')
+load_dotenv(dotenv_path=env_path)
 
-if DEBUG:
+from settings_files._global import *
 
-    env_path = Path('.') / '.env.debug'
-    load_dotenv(dotenv_path=env_path)
-    from settings_files._global import *
+if os.getenv('FLASK_ENV') == 'development':
     print(f'{Fore.WHITE + Back.YELLOW}We are in DEBUG mode{Style.RESET_ALL}')
 
 else:
-    env_path = Path('.') / '.env'
-    load_dotenv(dotenv_path=env_path)
-    from settings_files._global import *
     print(f'{Fore.WHITE + Back.RED}CAREFUL We are in PRODUCTION mode{Style.RESET_ALL}')
