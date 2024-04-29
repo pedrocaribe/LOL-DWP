7019be6bccc2eb4d5f1d1826ad1e73a1b1e86944
Pedro Caribe <pho.caribe@gmail.com>
2024-04-29 02:07:10 -0300

    added commenting

--- settings.py
+++ settings.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 from colorama import Back, Fore,Style
 
+# In Power Shell set the FLASK_ENV by using: $Env:FLASK_ENV = "development"
+# Check if it was set correctly by issuing: echo $Env:FLASK_ENV
 
 env_path = Path('.') / ('.env.debug' if os.getenv('FLASK_ENV') == 'development' else '.env')
 load_dotenv(dotenv_path=env_path)
