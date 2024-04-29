5d8828028ba67249de43724b40d73a8e96f99b5f
Pedro Caribe <pho.caribe@gmail.com>
2024-04-29 18:45:05 -0300

    working on debug environment

--- settings.py
+++ settings.py
@@ -6,13 +6,16 @@ from colorama import Back, Fore,Style
 # In Power Shell set the FLASK_ENV by using: $Env:FLASK_ENV = "development"
 # Check if it was set correctly by issuing: echo $Env:FLASK_ENV
 
-env_path = Path('.') / ('.env.debug' if os.getenv('FLASK_ENV') == 'development' else '.env')
-load_dotenv(dotenv_path=env_path)
-
-from settings_files._global import *
+current_env = os.getenv('FLASK_ENV', 'production')
+dotenv_path = Path('.') / ('.env.debug' if current_env == 'development' else '.env')
+print(f'Loading .ENV file from {dotenv_path}')
+load_dotenv(dotenv_path=dotenv_path)
 
 if os.getenv('FLASK_ENV') == 'development':
     print(f'{Fore.WHITE + Back.YELLOW}We are in DEBUG mode{Style.RESET_ALL}')
 
 else:
     print(f'{Fore.WHITE + Back.RED}CAREFUL We are in PRODUCTION mode{Style.RESET_ALL}')
+
+RIOT_TOKEN = os.getenv("RIOT_TOKEN", False)
+MAIL_PASSWD = os.getenv("MAIL_PASSWD", False)
